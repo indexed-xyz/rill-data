86f4dfec8cf2d3d4fd1f9ae8fd02e9b94118475a
Marissa Gorlick <marissagorlick@MacBook-Pro-2.lan>
2023-02-22 22:39:47 -0600

    refreshing data sources, models, and metrics

--- models/indexed_xyz_contract_transformation.sql
+++ models/indexed_xyz_contract_transformation.sql
@@ -6,6 +6,8 @@ SELECT
     ' - ',
     LENGTH(REGEXP_SPLIT_TO_ARRAY(STRING_SPLIT(event_signature, '(')[2], ','))
     ) AS event_type,
-  *, 
+  EPOCH_MS(block_time*1000) AS block_datetime,
+  CAST(block_number AS VARCHAR) AS block_number,
+  * EXCLUDE(block_number), 
 FROM indexed_xyz_contract_raw
--- WHERE address = '{{ address }}'
+WHERE address = '{{ address }}'
